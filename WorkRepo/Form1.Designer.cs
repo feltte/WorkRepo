528c07107d4e57965b0c91ea4c8051ba1ef7cecd
akr <akr@matu.skr.jp>
2020-08-18 19:59:56 +0900

    「シート内データ→配列」まで実装

---
+++ WorkRepo/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WorkRepo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBoxSourceFiles = new System.Windows.Forms.ListBox();
+			this.listBoxSheets = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// listBoxSourceFiles
+			// 
+			this.listBoxSourceFiles.AllowDrop = true;
+			this.listBoxSourceFiles.FormattingEnabled = true;
+			this.listBoxSourceFiles.ItemHeight = 12;
+			this.listBoxSourceFiles.Location = new System.Drawing.Point(12, 12);
+			this.listBoxSourceFiles.Name = "listBoxSourceFiles";
+			this.listBoxSourceFiles.Size = new System.Drawing.Size(650, 88);
+			this.listBoxSourceFiles.TabIndex = 0;
+			this.listBoxSourceFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBoxSourceFiles_DragDrop);
+			this.listBoxSourceFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxSourceFiles_DragEnter);
+			this.listBoxSourceFiles.DoubleClick += new System.EventHandler(this.listBoxSourceFiles_DoubleClick);
+			// 
+			// listBoxSheets
+			// 
+			this.listBoxSheets.FormattingEnabled = true;
+			this.listBoxSheets.ItemHeight = 12;
+			this.listBoxSheets.Location = new System.Drawing.Point(668, 12);
+			this.listBoxSheets.Name = "listBoxSheets";
+			this.listBoxSheets.Size = new System.Drawing.Size(120, 88);
+			this.listBoxSheets.TabIndex = 1;
+			this.listBoxSheets.DoubleClick += new System.EventHandler(this.listBoxSheets_DoubleClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listBoxSheets);
+			this.Controls.Add(this.listBoxSourceFiles);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBoxSourceFiles;
+		private System.Windows.Forms.ListBox listBoxSheets;
+	}
+}
+
---
+++ WorkRepo/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+using System.Diagnostics;
+
+namespace WorkRepo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void listBoxSourceFiles_DragEnter(object sender, DragEventArgs e)
+		{
+			var droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			listBoxSourceFiles.Items.AddRange(droppedFiles);
+
+		}
+
+		private void listBoxSourceFiles_DragDrop(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+				e.Effect = DragDropEffects.Copy;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
+		DataSet excelWorkBook;
+
+		private void listBoxSourceFiles_DoubleClick(object sender, EventArgs e)
+		{
+			var lb = sender as ListBox;
+			if (lb.SelectedIndex == -1) return;
+
+			var selectedFile = lb.SelectedItem.ToString();
+			if (!File.Exists(selectedFile))
+			{
+				MessageBox.Show("存在しないファイルでした", "エラー", MessageBoxButtons.OK);
+				listBoxSourceFiles.Items.Remove(selectedFile);
+				return;
+			}
+
+			using (var fs = File.Open(selectedFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			using (var er = ExcelReaderFactory.CreateReader(fs))
+			{
+				excelWorkBook = er.AsDataSet();
+				foreach (DataTable dt in excelWorkBook.Tables)
+				{
+					listBoxSheets.Items.Add(dt.TableName);
+				}
+			}
+		}
+
+		private void listBoxSheets_DoubleClick(object sender, EventArgs e)
+		{
+			var lb = sender as ListBox;
+			if (lb.SelectedIndex == -1) return;
+
+			var sheetName = lb.SelectedItem.ToString();
+			var dt = excelWorkBook.Tables[sheetName];
+			var rows = GetValuesAsArraysFrom(dt);
+
+			foreach(var obj in rows[4])
+			{
+				Trace.WriteLine($"{obj?.ToString()},  {obj?.GetType()}");
+			}
+		}
+
+		private object[][] GetValuesAsArraysFrom(DataTable dt)
+		{
+			var rows = new List<object[]>();
+			foreach (DataRow row in dt.Rows)
+			{
+				var elemtns = new List<object>();
+				for (var i = 0; i < dt.Columns.Count; i++)
+				{
+					elemtns.Add(row[i]);
+				}
+				rows.Add(elemtns.Select(obj=>obj is System.DBNull?null:obj).ToArray());
+			}
+			return rows.ToArray();
+		}
+
+	}
+}
